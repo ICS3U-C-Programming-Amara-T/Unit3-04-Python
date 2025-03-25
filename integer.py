497f5b8d506fc258239f947fda62ff524efda7a1
Amara Tie <amara.tie@stu.ocsb.ca>
2025-03-25 17:01:56 +0000

    integer.py

---
+++ integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created By: Amara Tie
+
+# Date: March 25, 2025
+
+# This is a positive/negative/zero program that asks the user for an integer.
+
+
+def main():
+    # Enter a positive or negative number
+    user_guess = int(input("Enter a postive or negative number: "))
+    print("")
+
+    # Check the user guess is correct
+    if user_guess < 0:
+        print(f"{user_guess} is a negative number!")
+    elif user_guess == 0:
+        print(f"{user_guess} is a zero.")
+
+    elif user_guess > 0:
+        print(f"{user_guess} is a positive number.")
+
+
+if __name__ == "__main__":
+    main()
